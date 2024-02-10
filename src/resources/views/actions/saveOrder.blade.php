13d979b0d590c0ecb7a1b172f522464590de625c
Jordi Puigdellívol <jordi.p@revo.works>
2024-02-10 16:04:15 +0100

    SAve order smaller

--- src/resources/views/actions/saveOrder.blade.php
+++ src/resources/views/actions/saveOrder.blade.php
@@ -1,3 +1,6 @@
 <x-ui::secondary-button class="hidden sm:block" action="async () => saveOrder('{{$resourceName}}', {{ $startAt }} )">
-    @icon(sort) {{ $title }}
+    <div class="flex items-center space-x-2">
+        <span>@icon(sort)</span>
+        <span class="hidden md:block">{{ $title }}</span>
+    </div>
 </x-ui::secondary-button>
