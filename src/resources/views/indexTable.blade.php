efb8f487b4530cf7a0e763f5356cdd27909b4371
Jordi Puigdellívol <jordi.p@revo.works>
2024-02-12 09:23:25 +0100

    Index table

--- src/resources/views/indexTable.blade.php
+++ src/resources/views/indexTable.blade.php
@@ -3,11 +3,11 @@
     <x-ui::table.table>
         <x-ui::table.header>
             <x-ui::table.row>
-            <x-ui::table.header-cell class="hidden sm:table-cell w-10 text-center">
+            <x-ui::table.header-cell class="hidden sm:table-cell w-8 pl-2 text-center">
                 <input type="checkbox" onclick="toggleSelectAll(this)">
             </x-ui::table.header-cell>
             @if ($sortable)
-                <x-ui::table.header-cell class="hidden w-4 text-center sm:table-cell" />
+                <x-ui::table.header-cell class="hidden w-8 text-center sm:table-cell" />
             @endif
             @foreach($fields as $field)
                 <x-ui::table.header-cell class="{{ $field->getRowCss() }}">
@@ -20,11 +20,11 @@
         <x-ui::table.body class="@if($sortable) sortable @endif">
             @foreach ($rows as $row)
                 <x-ui::table.row id="sort_{{$row->id}}" >
-                    <x-ui::table.cell class="w-10 text-center hidden sm:table-cell">
+                    <x-ui::table.cell class="pl-2 w-8 text-center hidden sm:table-cell">
                         <input class='actionCheckbox' type="checkbox" name="selected[{{$row->id}}]" meta:id="{{$row->id}}">
                     </x-ui::table.cell>
                     @if ($sortable)
-                        <x-ui::table.cell class="sort w-4 text-center hidden sm:table-cell text-gray-300 cursor-grab">
+                        <x-ui::table.cell class="sort w-8 text-center hidden sm:table-cell text-gray-300 cursor-grab">
                             @icon(grip-lines)
                         </x-ui::table.cell>
                     @endif
