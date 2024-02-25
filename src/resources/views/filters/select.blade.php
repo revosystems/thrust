24fc8577f703817ff982da465d676f4f95ea29cf
Jordi Puigdellívol <jordi.p@revo.works>
2024-02-25 17:12:24 +0100

    Nicer select

--- src/resources/views/filters/select.blade.php
+++ src/resources/views/filters/select.blade.php
@@ -1,6 +1,6 @@
-<x-ui::forms.select class="w-full" name="{{ $filter->class() }}" title="{{$filter->getTitle()}}">
+<x-ui::forms.searchable-select class="w-full min-w-60" name="{{ $filter->class() }}" title="{{$filter->getTitle()}}">
     <option value="--">--</option>
     @foreach ($filter->options() as $key => $optionValue)
         <option value="{{$optionValue}}" @if($value != null && $value != '--' && $value == $optionValue) selected @endif>{{$key}} </option>
     @endforeach
-</x-ui::forms.select>
+</x-ui::forms.searchable-select>
