7a434e0e3c41c44310667b80ca066ebcaf91aad3
Jordi Puigdellívol <jordi.p@revo.works>
2024-02-27 18:13:17 +0100

    Password field

--- src/resources/views/fields/input.blade.php
+++ src/resources/views/fields/input.blade.php
@@ -1,12 +1,25 @@
 <x-thrust::formField :field="$field" :title="$title" :description="$description" :aside="$showAside" :inline="$inline" :learnMoreUrl="$learnMoreUrl">
-    <x-ui::forms.text-input
-            icon="{{$icon}}"
-            id="{{$field}}"
-            name="{{$field}}"
-            type="{{$type}}"
-            value="{{$value}}"
-            placeholder="{{$title}}"
-            class="w-full"
-            {{ $attributes }}
-/>
+    @if($type == 'password')
+        <x-ui::forms.password
+                icon="{{$icon}}"
+                id="{{$field}}"
+                name="{{$field}}"
+                type="{{$type}}"
+                value="{{$value}}"
+                placeholder="{{$title}}"
+                class="w-full"
+                {{ $attributes }}
+        />
+    @else
+        <x-ui::forms.text-input
+                icon="{{$icon}}"
+                id="{{$field}}"
+                name="{{$field}}"
+                type="{{$type}}"
+                value="{{$value}}"
+                placeholder="{{$title}}"
+                class="w-full"
+                {{ $attributes }}
+        />
+    @endif
 </x-thrust::formField>
