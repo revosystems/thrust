ff035527844e1668a082bc59aa800d0373722643
Jordi Puigdellívol <jordi.p@revo.works>
2024-02-29 13:19:08 +0100

    Using new dejavu icons

--- src/resources/views/actions/main.blade.php
+++ src/resources/views/actions/main.blade.php
@@ -1,8 +1,5 @@
 <a class='{{$classes}}' href='{{$link}}'>
-    <x-ui::secondary-button>
-        <div class="flex items-center space-x-2">
-            @if($icon) <x-ui::icon>{{$icon}}</x-ui::icon>@endif
-            <div class="hidden sm:block">{{ $title }}</div>
-        </div>
+    <x-ui::secondary-button :icon="$icon" hideTextOnSm >
+        {{ $title }}
     </x-ui::secondary-button>
 </a>
--- src/resources/views/components/actions-index.blade.php
+++ src/resources/views/components/actions-index.blade.php
@@ -3,13 +3,10 @@
     @foreach($actions->where('main', false) as $action)
         <div class="">
             @if (count($action->fields()) == 0)
-                <x-ui::tertiary-button action="async () => {
+                <x-ui::tertiary-button :icon="$action->icon" action="async () => {
                 await runAction('{{ $action->getClassForJs() }}', '{{$action->needsConfirmation}}', '{{$action->needsSelection}}', '{{$action->getConfirmationMessage()}}')
             }">
-                    <div class="flex items-center space-x-2">
-                        <x-ui::icon class="text-gray-700">{{$action->icon}}</x-ui::icon>
-                        <div>{{ $action->getTitle() }}</div>
-                    </div>
+                    {{ $action->getTitle() }}
                 </x-ui::tertiary-button>
             @else
                 <a href="{{route('thrust.actions.create',[$resourceName])}}?action={{get_class($action)}}">
