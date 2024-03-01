a0b1244488d07b8dbbe2e38b335924bfd1c32554
Jordi Puigdellívol <jordi.p@revo.works>
2024-03-01 17:09:54 +0100

    Fixes action index

--- src/resources/views/components/actions-index.blade.php
+++ src/resources/views/components/actions-index.blade.php
@@ -6,13 +6,16 @@
                 <x-ui::tertiary-button :icon="$action->icon" action="async () => {
                 await runAction('{{ $action->getClassForJs() }}', '{{$action->needsConfirmation}}', '{{$action->needsSelection}}', '{{$action->getConfirmationMessage()}}')
             }">
-                    {{ $action->getTitle() }}
+                    <div class="flex items-center space-x-2">
+                        @if($action->icon)<x-ui::icon class="text-gray-700">{{$action->icon}}</x-ui::icon>@endif
+                        {{ $action->getTitle() }}
+                    </div>
                 </x-ui::tertiary-button>
             @else
-                <a href="{{route('thrust.actions.create',[$resourceName])}}?action={{get_class($action)}}">
-                    <x-ui::tertiary-button class='actionPopup' :async="false">
+                <a href="{{route('thrust.actions.create',[$resourceName])}}?action={{get_class($action)}}" class='actionPopup'>
+                    <x-ui::tertiary-button :async="false">
                         <div class="flex items-center space-x-2">
-                            <x-ui::icon class="text-gray-700">{{$action->icon}}</x-ui::icon>
+                            @if($action->icon)<x-ui::icon class="text-gray-700">{{$action->icon}}</x-ui::icon>@endif
                             <div>{{ $action->getTitle() }}</div>
                         </div>
                     </x-ui::tertiary-button >
