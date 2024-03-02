0fd30607089c3ad85dbe2f9e18d03b57ddff3594
Jordi Puigdellívol <jordi.p@revo.works>
2024-03-02 14:57:32 +0100

    Fixes double icon in actions-index

--- src/resources/views/components/actions-index.blade.php
+++ src/resources/views/components/actions-index.blade.php
@@ -6,18 +6,12 @@
                 <x-ui::tertiary-button :icon="$action->icon" action="async () => {
                 await runAction('{{ $action->getClassForJs() }}', '{{$action->needsConfirmation}}', '{{$action->needsSelection}}', '{{$action->getConfirmationMessage()}}')
             }">
-                    <div class="flex items-center space-x-2">
-                        @if($action->icon)<x-ui::icon class="text-gray-700">{{$action->icon}}</x-ui::icon>@endif
-                        {{ $action->getTitle() }}
-                    </div>
+                    {{ $action->getTitle() }}
                 </x-ui::tertiary-button>
             @else
                 <a href="{{route('thrust.actions.create',[$resourceName])}}?action={{get_class($action)}}" class='actionPopup'>
-                    <x-ui::tertiary-button :async="false">
-                        <div class="flex items-center space-x-2">
-                            @if($action->icon)<x-ui::icon class="text-gray-700">{{$action->icon}}</x-ui::icon>@endif
-                            <div>{{ $action->getTitle() }}</div>
-                        </div>
+                    <x-ui::tertiary-button :icon="$action->icon" :async="false">
+                        <div>{{ $action->getTitle() }}</div>
                     </x-ui::tertiary-button >
                 </a>
             @endif
