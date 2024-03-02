95af43ece05e2489057398838019b2bcb458d767
Jordi Puigdellívol <jordi.p@revo.works>
2024-03-02 14:37:29 +0100

    Some fixes

--- src/resources/views/actions/create.blade.php
+++ src/resources/views/actions/create.blade.php
@@ -1,18 +1,21 @@
-<div class="configForm">
-    <h2> {!! $action->getTitle()  !!}</h2>
-</div>
+<x-ui::breadcrums :data="[
+    $action->getTitle()
+]"/>
 
 <form action="{{route('thrust.actions.perform', [$resourceName])}}" method="POST">
     {{ csrf_field() }}
     <input type="hidden" name="ids" value="{{request('ids')}}">
     <input type="hidden" name="action" value="{{get_class($action)}}">
-    <div class="configForm">
+
+    <div class="flex flex-col divide-y divide-gray-100">
         @foreach($action->fields() as $field)
             {!! $field->displayInEdit(null) !!}
         @endforeach
     </div>
 
-    <button> {{ __("thrust::messages.perform") }} </button>
+    <x-ui::primary-button :async="true" type="submit">
+        {{ __("thrust::messages.perform") }}
+    </x-ui::primary-button>
 </form>
 
 <script>
--- src/resources/views/components/actions-index.blade.php
+++ src/resources/views/components/actions-index.blade.php
@@ -1,5 +1,5 @@
 @props(['actions', 'resourceName'])
-<div>
+<div id="thrust-resource-actions">
     @foreach($actions->where('main', false) as $action)
         <div class="">
             @if (count($action->fields()) == 0)
@@ -9,12 +9,9 @@
                     {{ $action->getTitle() }}
                 </x-ui::tertiary-button>
             @else
-                <a href="{{route('thrust.actions.create',[$resourceName])}}?action={{get_class($action)}}">
-                    <x-ui::tertiary-button class='actionPopup' :async="false">
-                        <div class="flex items-center space-x-2">
-                            <x-ui::icon class="text-gray-700">{{$action->icon}}</x-ui::icon>
-                            <div>{{ $action->getTitle() }}</div>
-                        </div>
+                <a href="{{route('thrust.actions.create',[$resourceName])}}?action={{get_class($action)}}" class='actionPopup'>
+                    <x-ui::tertiary-button :async="false" :icon="$action->icon">
+                        {{ $action->getTitle() }}
                     </x-ui::tertiary-button >
                 </a>
             @endif
