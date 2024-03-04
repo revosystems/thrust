5facfb85976b2cd46e93f2b51a2266c5fdb0ed47
Jordi Puigdellívol <jordi.p@revo.works>
2024-03-04 11:57:49 +0100

    Select can be searchable or not

--- src/resources/views/fields/select.blade.php
+++ src/resources/views/fields/select.blade.php
@@ -1,7 +1,7 @@
 <x-thrust::formField :field="$field" :title="$title" :description="$description" :aside="$showAside" :inline="$inline" :learnMoreUrl="$learnMoreUrl">
     <div class="flex flex-col">
         <div class="flex items-center space-x-2">
-            <x-ui::forms.searchable-select id="{{$field}}" name="{{$field}}" :icon="$icon ?? null" class="w-full">
+            <x-ui::forms.searchable-select id="{{$field}}" name="{{$field}}" :icon="$icon ?? null" class="w-full" :searchable="$searchable">
                 @if (isset($hasCategories) && $hasCategories))
                 @foreach($options as $category => $values)
                     <optgroup label="{{$category}}">
