3333c82486e025efda1a86ee44359d470ec0c24e
Jordi Puigdellívol <jordi.p@revo.works>
2024-03-27 09:47:20 +0100

    Global search with link to single resources

--- src/resources/views/globalSearch/index.blade.php
+++ src/resources/views/globalSearch/index.blade.php
@@ -1,38 +1,38 @@
 <div class="flex flex-col divide-y mt-4 gap-2">
     @forelse($found as $data)
         <div class="py-2">
-            <div class="font-semibold mb-2">
+            <div class="font-semibold text-lg mb-2">
                 @if($data['resource'] instanceof \BadChoice\Thrust\ChildResource)
-                    <div class="p-1">
-                        {{ $data['resource']->getTitle() }}
-                    </div>
+                    {{ $data['resource']->getTitle() }}
                 @elseif(isset($data['link']))
-                    <div class="hover:bg-neutral-50 rounded p-1">
-                        <a href="{{$data['link']}}">
-                            <div class="flex items-center justify-between">
-                                <div>{{$data['title']}}</div>
-                                <div class="text-sm text-gray-400">@icon(arrow-right)</div>
-                            </div>
-                        </a>
-                    </div>
+                    <a href="{{$data['link']}}">
+                        <div class="flex items-center justify-between">
+                            <div>{{$data['title']}}</div>
+                            <div class="text-sm text-gray-400">@icon(arrow-right)</div>
+                        </div>
+                    </a>
                 @else
-                    <div class="hover:bg-neutral-50 rounded p-1">
-                        <a href="{{route('thrust.index', $data['resource']->name())}}">
-                            <div class="flex items-center justify-between">
-                                <div>{{ $data['resource']->getTitle() }}</div>
-                                <div class="text-sm text-gray-400">@icon(arrow-right)</div>
-                            </div>
-                        </a>
-                    </div>
+                    <a href="{{route('thrust.index', $data['resource']->name())}}">
+                        <div class="flex items-center justify-between">
+                            <div>{{ $data['resource']->getTitle() }}</div>
+                            <div class="text-sm text-gray-400">@icon(arrow-right)</div>
+                        </div>
+                    </a>
                 @endif
             </div>
             @foreach($data['fields'] as $field)
-                <div class="text-gray-400 p-1">
+                @if($data['resource']::$singleResource)
+                    <a href="{{route('thrust.index', $data['resource']->name())}}?#{{ $field->field }}_div">
+                        @icon(caret-right){{ $field->getTitle() }}
+                    </a>
+                @else
+                <div class="text-gray-400 py-1">
                     @icon(caret-right){{ $field->getTitle() }}
                 </div>
+                @endif
             @endforeach
             @foreach($data['models'] as $model)
-                <div class="text-gray-700 hover:bg-neutral-50 rounded p-1">
+                <div class="text-gray-700">
                     <x-ui::a href="{{route('thrust.edit', [$data['resource']->name(), $model->id])}}" class="showPopup">
                         {{ $model->name ?? "something" }}
                     </x-ui::a>
