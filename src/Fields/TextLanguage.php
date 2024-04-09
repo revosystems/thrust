700358e58132e348280e34496de107d0ab778ab5
Jordi Puigdellívol <jordi.p@revo.works>
2024-04-09 10:02:12 +0200

    Working with translations

--- src/Fields/TextLanguage.php
+++ src/Fields/TextLanguage.php
@@ -6,10 +6,11 @@ class TextLanguage extends Text
 {
     protected $displayInIndexCallback = null;
     protected $languages;
-    public $showInIndex = false;
-    public $showInEdit  = true;
-    public $isTextArea  = false;
+    public bool $showInIndex = false;
+    public bool $showInEdit  = true;
+    public bool $isTextArea  = false;
     public $textAreaRows = 5;
+    protected ?string $icon = "language";
 
     public function languages($languages){
         $this->languages = $languages;
@@ -23,19 +24,17 @@ class TextLanguage extends Text
             'languages'       => $this->languages,
             'title'           => $this->getTitle(),
             'type'            => $this->getFieldType(),
-            'field'           => $this->field,
+            'field'            => $this->field,
             'value'           => $this->getValue($object),
             'validationRules' => $this->getHtmlValidation($object, $this->getFieldType()),
-            'attributes'      => $this->getFieldAttributes(),
+            'attributes'      => $this->getComponentBagAttributes($object),
             'description'     => $this->getDescription(),
             'isTextArea'      => $this->isTextArea,
             'textAreaRows'    => $this->textAreaRows,
+            'icon'            => $this->icon,
+            'learnMoreUrl'    => $this->learnMoreUrl,
+            'showAside'       => $this->showAside ?? $this->shouldShowAside(),
         ])->render();
-    }
-
-    public function getValue($object)
-    {
-        return Field::getValue($object);
     }
 
     public function isTextArea($rows = null)
@@ -44,4 +43,33 @@ class TextLanguage extends Text
         $this->textAreaRows = $rows == null ? $this->textAreaRows : $rows;
         return $this;
     }
+
+    public function mapAttributeFromRequest($value)
+    {
+        //dd($value);
+    }
+
+
+    public function getValue($object)
+    {
+        return $object->translations($this->field)->get()->filter()->mapWithKeys(function ($translation) {
+            return [$translation->language => $translation->text];
+        });
+    }
+
+
+    public function update($object, &$newData)
+    {
+        collect($newData[$this->field])->each(function($translation, $language) use($object) {
+            $object->translations($this->field)->updateOrCreate([
+                'language' => $language,
+                'field' => $this->field,
+            ],[
+                'text' => $translation
+            ]);
+        });
+
+        unset($newData[$this->field]);
+    }
+
 }
--- src/resources/views/fields/inputLanguage.blade.php
+++ src/resources/views/fields/inputLanguage.blade.php
@@ -1,30 +1,13 @@
-@component('thrust::components.formField' , ["field" => $field, "title" => $title, "description" => $description, "inline" => $inline])
-    @php $id = str_replace("]", "", str_replace("[", "", $field)) @endphp
-    <div class="thrust-language-selector-container" style="max-width:300px">
-        @foreach ($languages as $language)
-            <a class="thrust-language-selector languageSelector{{$id}}" id="{{$id}}languageSelector{{$language}}" onclick="changeLanguage('{{$id}}','{{$language}}')">{{ $language }}</a>
-        @endforeach
-    </div>
+<x-thrust::formField :field="$field" :title="$title" :description="$description" :aside="$showAside" :inline="$inline" :learnMoreUrl="$learnMoreUrl">
 
-    @foreach ($languages as $language)
-        @if( $isTextArea )
-            <textarea id="{{$id}}{{$language}}" name="{{$field}}[{{$language}}]" placeholder="{{$title}}" {{$attributes}} {!! $validationRules !!} @if($inline)
-            style="width:auto;" @endif class="hidden languageField{{$id}}" rows="{{$textAreaRows}}">{{$value[$language] ?? ""}}</textarea>
-        @else
-            <input type={{$type}} id="{{$id}}{{$language}}" value="{{$value[$language] ?? ""}}" name="{{$field}}[{{$language}}]" placeholder="{{$title}}"
-                   {{$attributes}} {!! $validationRules !!} @if($inline) style="width:auto;" @endif class="hidden languageField{{$id}}">
-        @endif
-    @endforeach
+    <x-ui::forms.translatable-text-input
+            :id="$field"
+            :name="$field"
+            :icon="$icon"
+            :values="$value"
+            placeholder="{{$title}}" {{$attributes}}
+            class="w-full"
+            {{ $attributes }}
+    />
 
-    @push('edit-scripts')
-        <script>
-            changeLanguage("{{$id}}", "{{$languages[0]}}");
-            function changeLanguage(field, language) {
-                $('.languageSelector'+field).css("background-color","#fff");
-                $('#'+field+'languageSelector'+language).css("background-color","#eee");
-                $('.languageField' + field).hide();
-                $('#'+field+language).removeClass('hidden').show();
-            }
-        </script>
-    @endpush
-@endcomponent
+</x-thrust::formField>
