b0abc3e939fb547c8012c19314b782e366457758
Jordi Puigdellívol <jordi.p@revo.works>
2024-04-22 13:38:05 +0200

    Icon tabs

--- src/resources/views/fields/tabs.blade.php
+++ src/resources/views/fields/tabs.blade.php
@@ -4,7 +4,14 @@
         @php $index = 1 @endphp
         @foreach($fields as $tab)
             <x-slot :name="'header'.$index">
-                {{ $tab->title }}
+                @if($tab->icon)
+                    <div class="flex items-center gap-1">
+                        <x-ui::icon>{{$tab->icon}}</x-ui::icon> <div class="hidden md:block">{{ $tab->title }}</div>
+                    </div>
+                @else
+                    {{ $tab->title }}
+                @endif
+
             </x-slot>
             <x-slot :name="'content'.$index">
                 <x-thrust::fields.edit-fields :object="$object" :fields="$tab->fields" />
