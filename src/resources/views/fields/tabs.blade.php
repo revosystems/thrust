e0e53d0a4661e6f5960d10e548ada5476753bb32
Jordi Puigdellívol <jordi.p@revo.works>
2024-04-23 15:51:02 +0200

    Improved tabs

--- src/resources/views/fields/tabs.blade.php
+++ src/resources/views/fields/tabs.blade.php
@@ -4,19 +4,23 @@
         @php $index = 1 @endphp
         @foreach($fields as $tab)
             <x-slot :name="'header'.$index">
-                @if($tab->icon)
-                    <div class="flex items-center gap-1">
-                        <x-ui::icon>{{$tab->icon}}</x-ui::icon> <div class="hidden md:block">{{ $tab->title }}</div>
-                    </div>
+                @if ($tab->icon)
+                    <x-ui::tooltip>
+                        <x-slot name="trigger">
+                            <x-ui::icon>{{$tab->icon}}</x-ui::icon>
+                        </x-slot>
+                        {{ $tab->title }}
+                    </x-ui::tooltip>
                 @else
                     {{ $tab->title }}
                 @endif
 
             </x-slot>
             <x-slot :name="'content'.$index">
+                <div class="text-lg my-2">{{ $tab->title }}</div>
                 <x-thrust::fields.edit-fields :object="$object" :fields="$tab->fields" />
             </x-slot>
-        @php $index++ @endphp
+            @php $index++ @endphp
         @endforeach
     </x-ui::tabs>
 </div>
