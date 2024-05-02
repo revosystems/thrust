5c7382b514dc1c37835ba6e9e79731fb6901674b
Jordi Puigdellívol <jordi.p@revo.works>
2024-05-02 09:38:04 +0200

    Fixes place inside the popup

--- src/resources/views/fields/place.blade.php
+++ src/resources/views/fields/place.blade.php
@@ -2,6 +2,13 @@
     https://www.webdew.com/blog/google-places-autocomplete-implementation
 --}}
 <x-thrust::formField :field="$field" :title="$title" :description="$description"  :inline="$inline">
+
+    <style>
+        .pac-container {
+            z-index: 1000000 !important;
+        }
+    </style>
+
     <div class="places">
         <x-ui::forms.text-input icon="location-dot" type="text" name="{{$field}}" id="{{$field}}" placeholder="{{$title}}" value="{{$value}}"  {{ $attributes->merge(['class' => 'w-full']) }}  autocomplete="off"/>
     </div>
