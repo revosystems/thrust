fb74a237a359b246efb8cde0a5d02c81277a5055
Jordi Puigdellívol <jordi.p@revo.works>
2024-05-23 11:56:24 +0200

    Fixes has many links as blue

--- src/resources/views/fields/hasMany.blade.php
+++ src/resources/views/fields/hasMany.blade.php
@@ -1,8 +1,8 @@
 @if ($withLink)
     @if($link)
-        <a href="{{ url($link) }}">
+        <a href="{{ url($link) }}" class="text-blue-400">
     @else
-        <a href="{{ route('thrust.hasMany', [$resourceName, $id , $relationship]) }}">
+        <a href="{{ route('thrust.hasMany', [$resourceName, $id , $relationship]) }}" class="text-blue-400">
     @endif
         @if($icon)
             <x-ui::icon>{{$icon}}</x-ui::icon> {{ $value }}
