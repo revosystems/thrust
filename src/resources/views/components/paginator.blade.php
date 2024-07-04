326ac2173b365c6b39aee56b4a428c328a4b67ae
Jordi Puigdellívol <jordi.p@revo.works>
2024-07-04 16:40:04 +0200

    Adding pagination selecto

--- src/resources/views/components/paginator.blade.php
+++ src/resources/views/components/paginator.blade.php
@@ -1,10 +1,21 @@
 @if ( method_exists($data, 'links')  && $data->lastPage() != 1)
-    <div class="m-4">
-        @php(
-            /*$data->withPath(Thrust::resourceNameFromModel($resource))*/
-            ""
-        )
-        {{ $data->links() }}
+    <div class="flex items-center gap-2 m-4">
+        <div class="grow">
+            @php(
+                /*$data->withPath(Thrust::resourceNameFromModel($resource))*/
+                ""
+            )
+            {{ $data->links() }}
+        </div>
+        <div x-data="">
+            <x-ui::forms.select name="pagination" x-on:change="
+                window.location = window.location.pathname + '?pagination=' + event.target.value;
+            ">
+                @foreach([25, 50, 100] as $value)
+                    <option value="{{ $value }}" @if(request('pagination') == $value) selected @endif>{{ $value }}</option>
+                @endforeach
+            </x-ui::forms.select>
+        </div>
     </div>
     @if(isset($popupLinks))
         <script>
