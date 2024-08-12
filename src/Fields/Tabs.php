765cb5b7dff03d5622a4236c202038a6c09bf4ff
Martí Sala Morral <marti.sala@revo.works>
2024-08-12 17:17:20 +0200

    fix: REV-16593 fields inside tabs are on edit now

--- src/Fields/Tabs.php
+++ src/Fields/Tabs.php
@@ -2,14 +2,19 @@
 
 namespace BadChoice\Thrust\Fields;
 
+use BadChoice\Thrust\Facades\Thrust;
+
 class Tabs extends Panel
 {
     public function displayInEdit($object, $inline = false)
     {
+        $resource = Thrust::make(Thrust::resourceNameFromModel($object));
+
         return view('thrust::fields.tabs', [
             'id' => $this->panelId,
             'fields' => $this->fields,
-            'object' => $object
+            'object' => $object,
+            'resource' => $resource,
         ]);
     }
 
