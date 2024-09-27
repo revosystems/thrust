1a1cb102461e398809f848ee9f74a890de9ee3c2
kiriamcf <kiriam.campobadal@revo.works>
2024-09-27 17:53:52 +0200

    bugfix: search text now also applies on resources based on$searchResource

--- src/Controllers/ThrustSearchController.php
+++ src/Controllers/ThrustSearchController.php
@@ -11,19 +11,25 @@ class ThrustSearchController extends Controller
     public function index($resourceName, $searchText)
     {
         request()->merge(['search' => $searchText]);
-        $resource = Thrust::make($resourceName)->setSearchText(request('search'));
+        $resource = Thrust::make($resourceName);
         if ($resource::$searchResource){        
             $resource = Thrust::make($resource::$searchResource);
         }
+
+        $resource->setSearchText(request('search'));
+
         return (new Index($resource))->show();
     }
 
     public function json($resourceName)
     {
-        $resource = Thrust::make($resourceName)->setSearchText(request('search'));
+        $resource = Thrust::make($resourceName);
         if ($resource::$searchResource){        
             $resource = Thrust::make($resource::$searchResource);
         }
+
+        $resource->setSearchText(request('search'));
+
         return response()->json($resource->query()->limit(50)->get()->map(function($object) use($resource) {
             return [
                 "id" => $object->id,
